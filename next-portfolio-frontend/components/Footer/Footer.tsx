d9b2d87ac6b3c93dab1878233814d953c0f2c4cf
Souvik Jana <souvikjana2013@gmail.com>
2022-07-09 21:38:35 +0530

    Fix: Remove props from timeline component for now.

--- next-portfolio-frontend/components/Footer/Footer.tsx
+++ next-portfolio-frontend/components/Footer/Footer.tsx
@@ -1,31 +1,28 @@
 const Footer = () => {
   return (
-    <>
-      <div className="flex flex-col space-y-8 bottom-0 py-4 md:space-y-0 md:flex-row">
-        <div className="flex-1 text-center md:text-left">
-          {`Let's Connect! I'd love if you reach out to me on any of the
+    <footer className="flex flex-col space-y-8 bottom-0 py-4 md:space-y-0 md:flex-row">
+      <div className="flex-1 text-center md:text-left">
+        {`Let's Connect! I'd love if you reach out to me on any of the
           platforms below. Drop me a message and I’ll get back to you ASAP!`}
-        </div>
-        <ul className="list-none flex flex-1 space-x-4 justify-center md:justify-end">
-          <li>
-            <i className="bx bxl-twitter bx-sm bx-border-circle"></i>
-          </li>
-          <li>
-            <i className="bx bxl-github bx-sm bx-border-circle"></i>
-          </li>
-          <li>
-            <i className="bx bxl-linkedin bx-sm bx-border-circle"></i>
-          </li>
-          <li>
-            <i className="bx bx-envelope bx-sm bx-border-circle"></i>
-          </li>
-          <li>
-            <i className="bx bxl-medium bx-sm bx-border-circle"></i>
-          </li>
-        </ul>
       </div>
-      <div></div>
-    </>
+      <ul className="list-none flex flex-1 space-x-4 justify-center md:justify-end">
+        <li>
+          <i className="bx bxl-twitter bx-sm bx-border-circle"></i>
+        </li>
+        <li>
+          <i className="bx bxl-github bx-sm bx-border-circle"></i>
+        </li>
+        <li>
+          <i className="bx bxl-linkedin bx-sm bx-border-circle"></i>
+        </li>
+        <li>
+          <i className="bx bx-envelope bx-sm bx-border-circle"></i>
+        </li>
+        <li>
+          <i className="bx bxl-medium bx-sm bx-border-circle"></i>
+        </li>
+      </ul>
+    </footer>
   );
 };
 
--- next-portfolio-frontend/components/Timeline/Timeline.tsx
+++ next-portfolio-frontend/components/Timeline/Timeline.tsx
@@ -6,7 +6,7 @@ interface TimelineProps {
   content: string;
 }
 
-const Timeline = ({ date, title, content }: TimelineProps) => {
+const Timeline = () => {
   return (
     <div className="antialiased p-6 bg-gray-100 text-gray-800">
       <div className="relative container mx-auto px-6 flex flex-col space-y-8">
@@ -22,6 +22,32 @@ const Timeline = ({ date, title, content }: TimelineProps) => {
           </div>
           <div className="timeline__container">
             <div className="timeline__pointer"></div>
+            <div className="bg-white rounded-md shadow-md p-6">
+              <span className="font-bold text-indigo-600 text-sm tracking-wide">
+                Jan 2021
+              </span>
+              <h1 className="text-2xl font-bold pt-1">An amazing travel</h1>
+              <p className="pt-1">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde,
+                nesciunt alias. Cumque velit sequi deleniti blanditiis nesciunt
+                incidunt accusamus tempore mollitia illo laudantium eveniet
+                veritatis, eum delectus asperiores, dolorem quibusdam?
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="relative z-10">
+          <div className="timeline__img p-5">
+            <Image
+              src="https://img.icons8.com/ios-filled/100/undefined/briefcase.png"
+              alt="work"
+              height={96}
+              width={96}
+            />
+          </div>
+          <div className="timeline__container timeline__container--left">
+            <div className="timeline__pointer timeline__pointer--left"></div>
             <div className="bg-white rounded-md shadow-md p-6">
               <span className="font-bold text-indigo-600 text-sm tracking-wide">
                 Jan 2021
