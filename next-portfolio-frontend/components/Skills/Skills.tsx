ff7965a700e78ea65eb9aa46f2be9163bec027a4
Souvik Jana <souvikjana2013@gmail.com>
2022-07-30 22:58:33 +0530

    refactor: add types to skills and update about page

--- next-portfolio-frontend/components/Skills/Skills.tsx
+++ next-portfolio-frontend/components/Skills/Skills.tsx
@@ -1,23 +1,29 @@
-import React from "react";
+interface SkillsProps {
+  skillsData: Record<string, Array<string>>;
+}
 
-const Skills = ({ skillsObject }: any) => {
+interface SkillTypeProps extends SkillsProps {
+  type: string;
+}
+
+interface IndividualSkillProps {
+  skillsArray: Array<string>;
+}
+
+const Skills = ({ skillsData }: SkillsProps) => {
   return (
     <>
       <h1 className="text-2xl uppercase font-bold py-8">Skills</h1>
       <ul>
-        {Object.keys(skillsObject).map((skillType) => (
-          <SkillType
-            key={skillType}
-            skillsObject={skillsObject}
-            skillType={skillType}
-          />
+        {Object.keys(skillsData).map((type) => (
+          <SkillType key={type} skillsData={skillsData} type={type} />
         ))}
       </ul>
     </>
   );
 };
 
-const IndividualSkill = ({ skillsArray }) => {
+const IndividualSkill = ({ skillsArray }: IndividualSkillProps) => {
   return (
     <>
       {skillsArray.map((skill) => {
@@ -31,12 +37,12 @@ const IndividualSkill = ({ skillsArray }) => {
   );
 };
 
-const SkillType = ({ skillsObject, skillType }) => {
+const SkillType = ({ skillsData, type }: SkillTypeProps) => {
   return (
-    <div key={skillType} className="ml-4 py-4">
-      <h2 className="text-lg">{skillType}</h2>
-      <ul className="list-disc flex">
-        <IndividualSkill skillsArray={skillsObject[skillType]} />
+    <div key={type} className="ml-4 py-4">
+      <h2 className="text-lg">{type}</h2>
+      <ul className="list-disc flex flex-wrap">
+        <IndividualSkill skillsArray={skillsData[type]} />
       </ul>
     </div>
   );
