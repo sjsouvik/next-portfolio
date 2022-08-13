dc58267f92ffa382f4e2ee305ad27cb72d7fa5e2
Souvik Jana <souvikjana2013@gmail.com>
2022-08-13 21:09:31 +0530

    feat: add social media links to footer

--- next-portfolio-frontend/components/Footer/Footer.tsx
+++ next-portfolio-frontend/components/Footer/Footer.tsx
@@ -1,27 +1,51 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
-    <footer className="flex flex-col space-y-8 bottom-0 py-4 pr-4 lg:pr-0 max-w-4xl md:space-y-0 md:flex-row">
-      <div className="flex-1 text-center md:text-left">
-        {`Let's Connect! I'd love if you reach out to me on any of the
-          platforms below. Drop me a message and I’ll get back to you ASAP!`}
+    <footer className="absolute bg-gray-800 bottom-0 left-0 right-0 py-6">
+      <div className="bg-gray-800 py-6 text-white px-4 max-w-4xl m-auto flex flex-col space-y-8 md:space-y-0 md:flex-row lg:px-0">
+        <div className="flex-1 text-center mr-4 md:text-left">
+          {`Let's Connect I'd love if you reached out to me on any of the
+          platforms below. Drop me a line and I’ll get back to you ASAP!`}
+        </div>
+        <ul className="list-none flex flex-1 space-x-4 justify-center md:justify-end">
+          <li>
+            <Link href="https://twitter.com/sjsouvik">
+              <a className="text-white" target="_blank">
+                <i className="bx bxl-twitter bx-sm bx-border-circle bg-gray-400 hover:bg-gray-600"></i>
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="https://github.com/sjsouvik">
+              <a className="text-white" target="_blank">
+                <i className="bx bxl-github bx-sm bx-border-circle bg-gray-400 hover:bg-gray-600"></i>
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="https://www.linkedin.com/in/souvik-jana/">
+              <a className="text-white" target="_blank">
+                <i className="bx bxl-linkedin bx-sm bx-border-circle bg-gray-400 hover:bg-gray-600"></i>
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="mailto:souvikjana2013@gmail.com">
+              <a className="text-white" target="_blank">
+                <i className="bx bx-envelope bx-sm bx-border-circle bg-gray-400 hover:bg-gray-600"></i>
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="https://sjsouvik.medium.com">
+              <a className="text-white" target="_blank">
+                <i className="bx bxl-medium bx-sm bx-border-circle bg-gray-400 hover:bg-gray-600"></i>
+              </a>
+            </Link>
+          </li>
+        </ul>
       </div>
-      <ul className="list-none flex flex-1 space-x-4 justify-center md:justify-end">
-        <li>
-          <i className="bx bxl-twitter bx-sm bx-border-circle"></i>
-        </li>
-        <li>
-          <i className="bx bxl-github bx-sm bx-border-circle"></i>
-        </li>
-        <li>
-          <i className="bx bxl-linkedin bx-sm bx-border-circle"></i>
-        </li>
-        <li>
-          <i className="bx bx-envelope bx-sm bx-border-circle"></i>
-        </li>
-        <li>
-          <i className="bx bxl-medium bx-sm bx-border-circle"></i>
-        </li>
-      </ul>
     </footer>
   );
 };
--- next-portfolio-frontend/pages/projects.tsx
+++ next-portfolio-frontend/pages/projects.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import {  
+import {
   getFilteredProjectsBasedOnTag,
   getTagsFromAllProjects,
   pluralize,
@@ -7,12 +7,14 @@ import {
 import { Card, Layout, SectionHeading } from "../components";
 
 const Projects = () => {
-  const [selectedTag, setSelectedTag] = useState("");  
+  const [selectedTag, setSelectedTag] = useState("");
 
-  const handleTagClick = (tagName: string) => {    
-    setSelectedTag((prevSelectedTag) => prevSelectedTag === tagName ? '': tagName);    
+  const handleTagClick = (tagName: string) => {
+    setSelectedTag((prevSelectedTag) =>
+      prevSelectedTag === tagName ? "" : tagName
+    );
   };
-  
+
   const filteredProjects = getFilteredProjectsBasedOnTag(selectedTag);
 
   const filteredProjectsLength = filteredProjects.length;
@@ -21,7 +23,7 @@ const Projects = () => {
 
   return (
     <Layout>
-      {selectedTag && <SectionHeading heading={headingText}/>}
+      {selectedTag && <SectionHeading heading={headingText} />}
       <div className="flex flex-col justify-between lg:flex-row mb-24">
         <ul className="grid grid-cols-1 sm:grid-cols-2 gap-6 lg:gap-8 mr-4 flex-1">
           {filteredProjects.map((project) => (
@@ -34,7 +36,9 @@ const Projects = () => {
             {getTagsFromAllProjects().map(([tagName, tagCount]) => (
               <li
                 key={tagName}
-                className={`m-1 text-sm px-3 py-1 border border-gray-500 rounded-full cursor-pointer hover:bg-blue-600 hover:text-white ${selectedTag === tagName ? 'bg-blue-600 text-white' : ''}`}
+                className={`m-1 text-sm px-3 py-1 border border-gray-500 rounded-full cursor-pointer hover:bg-blue-600 hover:text-white ${
+                  selectedTag === tagName ? "bg-blue-600 text-white" : ""
+                }`}
                 onClick={() => handleTagClick(tagName)}
               >
                 {tagName}, {tagCount}
